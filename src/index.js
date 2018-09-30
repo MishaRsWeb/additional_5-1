6f0491a50e91b75f52d03945461f17d8d90d05f9
michael.rasskazov <misharasskazov1@gmail.com>
2018-09-30 15:30:04 +0400

    CheckBrackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets = [];
+  let closeBrackets = [];
+
+  bracketsConfig.forEach(function(e) {
+      openBrackets.push(e[0]);
+      closeBrackets.push(e[1]);
+  });
+
+  let check = [];
+
+  str.split('').forEach(function(e) {
+      if (openBrackets.includes(e) && !closeBrackets.includes(e)) {
+          check.push(e);
+      }
+      if (closeBrackets.includes(e)) {
+          if (check[check.length - 1] == openBrackets[closeBrackets.indexOf(e)]) {
+              check.pop();
+          } else {
+              check.push(e);
+          }
+      }
+  });
+
+  return !check.join('');
 }
+
